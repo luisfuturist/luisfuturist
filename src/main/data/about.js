3399cb268607e8a65563df2e4765d1a82d425f25
Luis Float <contact@luisfloat.com>
2022-06-25 19:16:31 -0300

    add title to about session

--- src/main/data/about.js
+++ src/main/data/about.js
@@ -1,4 +1,5 @@
 export default {
+    name: "About",
     title: "Honor2045",
     heading: "Hey there, it's Honor!",
     paragraph: "You can reach me at <strong>ctc@honor2045.com</strong> or any social media.<br> I'm interested in <strong>Software Engineering</strong>.<br> Currently learning <strong>Web Technologies</strong>.",
