81e3c92c5a3293efa21d008ddc5de6b4e5e435c1
Luis Float <contact@luisfloat.com>
2022-07-07 23:18:57 -0300

    fix(about): change title value

--- src/main/data/about.js
+++ src/main/data/about.js
@@ -1,6 +1,6 @@
 export default {
     name: "About",
-    title: "Luis",
+    title: "Luis Float",
     heading: "Hey there, it's Luis!",
     paragraph: "You can reach me at <strong>contact@luisfloat.com</strong> or any social media.<br> I'm interested in <strong>Software Engineering</strong>.<br> Currently learning <strong>Web Technologies</strong>.",
     note: "<strong>Note:</strong> I also have interest in Philosophy, Digital Design and Scifi Art."
