7af0634697529d77d68614e14d5a7e6c994500d1
Luis Float <contact@luisfloat.com>
2022-07-14 02:16:54 -0300

    feat(tokens): add colors

---
+++ src/main/tokens.js
@@ -0,0 +1,17 @@
+const colorOptions = {
+    gray2: "333333",
+    azure3: "005c99",
+};
+const colorDecisions = {
+    badge: {
+        normal: colorOptions.gray2,
+        primary: colorOptions.azure3,
+    },
+};
+
+let colors = {
+    options: colorOptions,
+    decisions: colorDecisions,
+};
+
+export { colors };
