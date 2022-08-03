1651595f62d0e5ddad146334417204b6c0d01eab
Luis Float <contact@luisfloat.com>
2022-08-03 02:13:24 -0300

    refactor(main): use `_badge` factory in workspace data

--- src/main/data/workspace.js
+++ src/main/data/workspace.js
@@ -1,43 +1,14 @@
 import { colors } from "../tokens.js";
+import { _badge } from "../utils/factory.js";
 const badgeColor = colors.decisions.badge.normal;
 
 export default {
     name: "Workspace",
     items: [
-        {
-            link: "https://code.visualstudio.com/",
-            alt: "Visual Studio Code",
-            name: "Vscode",
-            color: badgeColor,
-            logo: "visualstudio"
-        },
-        {
-            link: "https://google.com/chrome",
-            alt: "Chrome",
-            name: "Chrome",
-            color: badgeColor,
-            logo: "chrome"
-        },
-        {
-            link: "https://ubuntu.com",
-            alt: "Ubuntu",
-            name: "Ubuntu",
-            color: badgeColor,
-            logo: "ubuntu"
-        },
-        {
-            link: "https://android.com",
-            alt: "Android",
-            name: "Android",
-            color: badgeColor,
-            logo: "android"
-        },
-        {
-            link: "https://apps.google.com",
-            alt: "Google Apps",
-            name: "Google Apps",
-            color: badgeColor,
-            logo: "google"
-        }
-    ]
+        _badge("Vscodee", "https://code.visualstudio.com/", "Vscode", badgeColor, "visualstudio"),
+        _badge("Chrome", "https://google.com/chrome", "Chrome", badgeColor, "chrome"),
+        _badge("Ubuntu", "https://ubuntu.com", "Ubuntu", badgeColor, "ubuntu"),
+        _badge("Android", "https://android.com", "Android", badgeColor, "android"),
+        _badge("Google Apps", "https://apps.google.com", "Google Apps", badgeColor, "google"),
+    ],
 };
