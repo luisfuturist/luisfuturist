55dfec8cd78909455e5cef1091420e357a2de669
Luis Float <contact@luisfloat.com>
2022-08-03 01:23:59 -0300

    feat(main): add `_badgeLabel` factory function

--- src/main/utils/factory.js
+++ src/main/utils/factory.js
@@ -8,4 +8,17 @@ function _badge(name, link, alt, color, logo) {
     };
 }
 
-export { _badge };
+function _badgeLabel(name, label, link, alt, color) {
+    return {
+        name: name,
+        label: label,
+        link: link,
+        color: color,
+        alt: alt,
+    };
+}
+
+export {
+    _badge,
+    _badgeLabel,
+};
