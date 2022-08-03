91adaa970b04c1b8a8b1ed3d71dc8b50c4c94de1
Luis Float <contact@luisfloat.com>
2022-08-03 01:50:26 -0300

    refactor(main): use `_badgeLabel` factory in stati data

--- src/main/data/stati.js
+++ src/main/data/stati.js
@@ -1,29 +1,12 @@
 import { colors } from "../tokens.js";
+import { _badgeLabel } from "../utils/factory.js";
 const badgeColor = colors.decisions.badge.primary;
 
 export default {
     name: "Luis",
     items: [
-        {
-            link: "https://github.com/luisfloat",
-            label: "working",
-            name: "freelancing; open to work",
-            color: badgeColor,
-            alt: "working status"
-        },
-        {
-            link: "https://github.com/luisfloat",
-            label: "learning",
-            name: "autodidactically",
-            color: badgeColor,
-            alt: "learning status"
-        },
-        {
-            link: "https://www.google.com/maps/place/State+of+Santa+Catarina",
-            label: "living",
-            name: "Brazil, SC",
-            color: badgeColor,
-            alt: "living status" 
-        }
-    ]
+        _badgeLabel("freelancing; open to work", "working", "https://github.com/luisfloat", "working status", badgeColor),
+        _badgeLabel("autodidactically", "learning", "https://github.com/luisfloat", "learning status", badgeColor),
+        _badgeLabel("Brazil, SC", "living", "https://www.google.com/maps/place/State+of+Santa+Catarina", "living status", badgeColor),
+    ],
 };
