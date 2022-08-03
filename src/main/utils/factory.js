79d2e0214b867e38270078b077991a5494d819c1
Luis Float <contact@luisfloat.com>
2022-08-02 23:45:57 -0300

    feat(main): add utils for data factoring

---
+++ src/main/utils/factory.js
@@ -0,0 +1,11 @@
+function _badge(name, link, alt, color, logo) {
+    return {
+        name: name,
+        link: link,
+        alt: alt,
+        color: color,
+        logo: logo,
+    };
+}
+
+export { _badge };
