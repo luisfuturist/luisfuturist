9afd4390944bd7203fefc823207923b10f8fa300
Luis Float <contact@luisfloat.com>
2022-08-11 00:12:19 -0300

    bug(main): fix vscode display name

--- src/main/data/workspace.js
+++ src/main/data/workspace.js
@@ -5,7 +5,7 @@ const badgeColor = colors.decisions.badge.normal;
 export default {
     name: "Workspace",
     items: [
-        _badge("Vscodee", "https://code.visualstudio.com/", "Vscode", badgeColor, "visualstudio"),
+        _badge("Visual Studio Code", "https://code.visualstudio.com/", "Vscode", badgeColor, "visualstudio"),
         _badge("Chrome", "https://google.com/chrome", "Chrome", badgeColor, "chrome"),
         _badge("Ubuntu", "https://ubuntu.com", "Ubuntu", badgeColor, "ubuntu"),
         _badge("Android", "https://android.com", "Android", badgeColor, "android"),
