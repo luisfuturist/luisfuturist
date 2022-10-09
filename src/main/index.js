ebadaa853e8c9b60c36de4a7ebe7dc951d8bdcc3
Luis Float <contact@luisfloat.com>
2022-10-08 23:59:22 -0300

    refactor(main): add about session

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,11 @@
-import { Markdown } from '@scdev/declarative-markdown';
+import { Markdown, quote } from '@scdev/declarative-markdown';
 import content from './content/index.js';
 
 const md = new Markdown(content.about.title);
+
+md
+.header(content.about.name, 3)
+.header(content.about.heading, 4)
+.paragraph(content.about.paragraph + "\n" + quote(content.about.note))
 
 export default md;
