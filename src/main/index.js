946d182c579877679ba0443b533d584a6ab2b8ee
Luis Float <contact@luisfloat.com>
2022-10-09 00:57:25 -0300

    refactor(main): add status items

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,12 @@
 import { inlineCode, Markdown, quote } from '@scdev/declarative-markdown';
+import { inlineBadges } from './utils/shields.js';
 import content from './content/index.js';
 
 const md = new Markdown(content.about.title);
 
 md
+.paragraph(inlineBadges(content.stati.items))
+
 .header(content.about.name, 3)
 .header(content.about.heading, 4)
 .paragraph(content.about.paragraph + "\n" + quote(content.about.note))
